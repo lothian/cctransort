3847bf143cb39cb05523bd6480717d05a1270ec9
T. Daniel Crawford <crawdad@vt.edu>
2015-12-26 19:46:54 -0500

    Added integral transforms and basic sorts.  Working on DPD_ID().

--- cctransort.cc
+++ cctransort.cc
@@ -31,6 +31,8 @@
 #include <libtrans/integraltransform.h>
 #include <libdpd/dpd.h>
 
+#define ID(x) ints->DPD_ID(x)
+
 using namespace boost;
 
 namespace psi{ namespace cctransort {
@@ -56,7 +58,8 @@ PsiReturnType cctransort(Options& options)
   if(!ref) throw PSIEXCEPTION("SCF has not been run yet!");
 
   std::vector<shared_ptr<MOSpace> > spaces;
-  spaces.push_back(MOSpace::all);
+  spaces.push_back(MOSpace::occ);
+  spaces.push_back(MOSpace::vir);
 
   shared_ptr<IntegralTransform> ints;
   if(options.get_str("REFERENCE") == "RHF")
@@ -68,25 +71,123 @@ PsiReturnType cctransort(Options& options)
   else
     throw PSIEXCEPTION("Invalid choice of reference wave function.");
 
-  ints->transform_tei(MOSpace::all, MOSpace::all, MOSpace::all, MOSpace::all);
+
+  shared_ptr<PSIO> psio(_default_psio_lib_);
+  dpdbuf4 K;
+  dpd_set_default(ints->get_dpd_id());
+  ints->set_keep_dpd_so_ints(true);
+
+  outfile->Printf("[O,O] = %d\n", ID("[O,O]"));
+  outfile->Printf("[V,V] = %d\n", ID("[V,V]"));
+  outfile->Printf("[O,V] = %d\n", ID("[O,V]"));
+  outfile->Printf("[V,O] = %d\n", ID("[V,O]"));
+  outfile->Printf("[O>=O]+ = %d\n", ID("[O>=O]+"));
+  outfile->Printf("[V>=V]+ = %d\n", ID("[V>=V]+"));
+  outfile->Printf("[O>O]- = %d\n", ID("[O>O]-"));
+  outfile->Printf("[V>V]- = %d\n", ID("[V>V]-"));
+
+  ints->print_dpd_lookup();
+
+  ints->transform_tei(MOSpace::occ, MOSpace::occ, MOSpace::occ, MOSpace::occ, IntegralTransform::MakeAndKeep);
+  ints->transform_tei(MOSpace::occ, MOSpace::occ, MOSpace::occ, MOSpace::vir, IntegralTransform::ReadAndKeep);
+  ints->transform_tei(MOSpace::occ, MOSpace::occ, MOSpace::vir, MOSpace::vir, IntegralTransform::ReadAndNuke);
+  ints->transform_tei(MOSpace::occ, MOSpace::vir, MOSpace::occ, MOSpace::vir, IntegralTransform::MakeAndKeep);
+  ints->transform_tei(MOSpace::occ, MOSpace::vir, MOSpace::vir, MOSpace::vir, IntegralTransform::ReadAndNuke);
+  ints->transform_tei(MOSpace::vir, MOSpace::vir, MOSpace::vir, MOSpace::vir, IntegralTransform::MakeAndNuke);
+
   double efzc = ints->get_frozen_core_energy();
   outfile->Printf("Frozen core energy = %20.12f\n", efzc);
 
-  shared_ptr<PSIO> psio(_default_psio_lib_);
-  psio->open(PSIF_LIBTRANS_DPD, PSIO_OPEN_OLD);
-  dpd_set_default(ints->get_dpd_id());
-  dpdbuf4 K;
-  global_dpd_->buf4_init(&K, PSIF_LIBTRANS_DPD, 0, ID("[A,A]"), ID("[A,A]"), ID("[A>=A]+"), ID("[A>=A]+"), 0, "MO Ints (AA|AA)");
-  for(int h=0; h < ref->nirrep(); h++) {
-    global_dpd_->buf4_mat_irrep_row_init(&K, h);
-    for(int pq=0; pq < K.params->rowtot[h]; pq++) {
-      global_dpd_->buf4_mat_irrep_row_rd(&K, h);
+  // Transformations are complete; need to re-set the 
 
-    }
-    global_dpd_->buf4_mat_irrep_row_close(&K, h);
-  }
+  psio->open(PSIF_LIBTRANS_DPD, PSIO_OPEN_OLD);
+
+  psio->open(PSIF_CC_AINTS, PSIO_OPEN_OLD);
+  global_dpd_->buf4_init(&K, PSIF_LIBTRANS_DPD, 0, ID("[O,O]"), ID("[O,O]"), ID("[O>=O]+"), ID("[O>=O]+"), 0, "MO Ints (OO|OO)");
+  global_dpd_->buf4_sort(&K, PSIF_CC_AINTS, prqs, ID("[O,O]"), ID("[O,O]"), "A <ij|kl>");
   global_dpd_->buf4_close(&K);
+  psio->close(PSIF_CC_AINTS, 1);
+
+  psio->open(PSIF_CC_BINTS, PSIO_OPEN_OLD);
+  global_dpd_->buf4_init(&K, PSIF_LIBTRANS_DPD, 0, ID("[V,V]"), ID("[V,V]"), ID("[V>=V]+"), ID("[V>=V]+"), 0, "MO Ints (VV|VV)");
+  global_dpd_->buf4_sort(&K, PSIF_CC_BINTS, prqs, ID("[V,V]"), ID("[V,V]"), "B <ab|cd>");
+  global_dpd_->buf4_close(&K);
+  psio->close(PSIF_CC_BINTS, 1);
+
+  psio->open(PSIF_CC_CINTS, PSIO_OPEN_OLD);
+  global_dpd_->buf4_init(&K, PSIF_LIBTRANS_DPD, 0, ID("[O,O]"), ID("[V,V]"), ID("[O>=O]+"), ID("[V>=V]+"), 0, "MO Ints (OO|VV)");
+  global_dpd_->buf4_sort(&K, PSIF_CC_CINTS, prqs, ID("[O,V]"), ID("[O,V]"), "C <ia|jb>");
+  global_dpd_->buf4_close(&K);
+  psio->close(PSIF_CC_CINTS, 1);
+
+  psio->open(PSIF_CC_DINTS, PSIO_OPEN_OLD);
+  global_dpd_->buf4_init(&K, PSIF_LIBTRANS_DPD, 0, ID("[O,V]"), ID("[O,V]"), ID("[O,V]"), ID("[O,V]"), 0, "MO Ints (OV|OV)");
+  global_dpd_->buf4_sort(&K, PSIF_CC_DINTS, prqs, ID("[O,O]"), ID("[V,V]"), "D <ij|ab>");
+  global_dpd_->buf4_close(&K);
+  psio->close(PSIF_CC_DINTS, 1);
+
+  psio->open(PSIF_CC_EINTS, PSIO_OPEN_OLD);
+  global_dpd_->buf4_init(&K, PSIF_LIBTRANS_DPD, 0, ID("[O,O]"), ID("[O,V]"), ID("[O>=O]+"), ID("[O,V]"), 0, "MO Ints (OO|OV)");
+  global_dpd_->buf4_sort(&K, PSIF_CC_EINTS, sqrp, ID("[V,O]"), ID("[O,O]"), "E <ai|jk>");
+  global_dpd_->buf4_close(&K);
+  psio->close(PSIF_CC_EINTS, 1);
+
+  psio->open(PSIF_CC_FINTS, PSIO_OPEN_OLD);
+  global_dpd_->buf4_init(&K, PSIF_LIBTRANS_DPD, 0, ID("[O,V]"), ID("[V,V]"), ID("[O,V]"), ID("[V>=V]+"), 0, "MO Ints (OV|VV)");
+  global_dpd_->buf4_sort(&K, PSIF_CC_FINTS, prqs, ID("[O,V]"), ID("[V,V]"), "D <ia|bc>");
+  global_dpd_->buf4_close(&K);
+  psio->close(PSIF_CC_FINTS, 1);
+
   psio->close(PSIF_LIBTRANS_DPD, 1);
+
+  // Additional sorts to mimic ccsort behavior
+  for(int i=PSIF_CC_MIN; i <= PSIF_CC_MAX; i++) psio->open(i, PSIO_OPEN_OLD);
+  dpdbuf4 C, D;
+
+  // C_SORT() 
+  // <ia||jb> = <ia|jb> - <ia|bj> = <ia|jb> - <ij|ba>
+  global_dpd_->buf4_init(&C, PSIF_CC_CINTS, 0, ID("[O,V]"), ID("[O,V]"), ID("[O,V]"), ID("[O,V]"), 0, "C <ia|jb>");
+  global_dpd_->buf4_copy(&C, PSIF_CC_CINTS, "C <ia||jb>");
+  global_dpd_->buf4_close(&C);
+
+  global_dpd_->buf4_init(&D, PSIF_CC_DINTS, 0, ID("[O,O]"), ID("[V,V]"), ID("[O,O]"), ID("[V,V]"), 0, "D <ij|ab>");
+  global_dpd_->buf4_sort(&D, PSIF_CC_TMP0, psqr, ID("[O,V]"), ID("[O,V]"), "D <ij|ab> (ib,ja)");
+  global_dpd_->buf4_close(&D);
+  global_dpd_->buf4_init(&D, PSIF_CC_TMP0, 0, ID("[O,V]"), ID("[O,V]"), ID("[O,V]"), ID("[O,V]"), 0, "D <ij|ab> (ib,ja)");
+  global_dpd_->buf4_init(&C, PSIF_CC_CINTS, 0, ID("[O,V]"), ID("[O,V]"), ID("[O,V]"), ID("[O,V]"), 0, "C <ia||jb>");
+  global_dpd_->buf4_axpy(&D, &C, -1);
+  global_dpd_->buf4_close(&D);
+  global_dpd_->buf4_close(&C);
+
+  /* <ia|jb> (bi,ja) */
+  global_dpd_->buf4_init(&C, PSIF_CC_CINTS, 0, ID("[O,V]"), ID("[O,V]"), ID("[O,V]"), ID("[O,V]"), 0, "C <ia|jb>");
+  global_dpd_->buf4_sort(&C, PSIF_CC_CINTS, sprq, ID("[V,O]"), ID("[O,V]"), "C <ia|jb> (bi,ja)");
+  global_dpd_->buf4_close(&C);
+
+  /* <ia||jb> (bi,ja) */
+  global_dpd_->buf4_init(&C, PSIF_CC_CINTS, 0, ID("[O,V]"), ID("[O,V]"), ID("[O,V]"), ID("[O,V]"), 0, "C <ia||jb>");
+  global_dpd_->buf4_sort(&C, PSIF_CC_CINTS, sprq, ID("[V,O]"), ID("[O,V]"), "C <ia||jb> (bi,ja)");
+  global_dpd_->buf4_close(&C);
+
+  /* <ia|jb> (ia,bj) */
+  global_dpd_->buf4_init(&C, PSIF_CC_CINTS, 0, ID("[O,V]"), ID("[O,V]"), ID("[O,V]"), ID("[O,V]"), 0, "C <ia|jb>");
+  global_dpd_->buf4_sort(&C, PSIF_CC_CINTS, pqsr, ID("[O,V]"), ID("[V,O]"), "C <ia|jb> (ia,bj)");
+  global_dpd_->buf4_close(&C);
+
+  /* <ia||jb> (ia,bj) (Wmbej.c) */
+  global_dpd_->buf4_init(&C, PSIF_CC_CINTS, 0, ID("[O,V]"), ID("[O,V]"), ID("[O,V]"), ID("[O,V]"), 0, "C <ia||jb>");
+  global_dpd_->buf4_sort(&C, PSIF_CC_CINTS, pqsr, ID("[O,V]"), ID("[V,O]"), "C <ia||jb> (ia,bj)");
+  global_dpd_->buf4_close(&C);
+
+  /* <ai|bj> (cchbar/Wabei_RHF.c) */
+  global_dpd_->buf4_init(&C, PSIF_CC_CINTS, 0, ID("[O,V]"), ID("[O,V]"), ID("[O,V]"), ID("[O,V]"), 0, "C <ia|jb>");
+  global_dpd_->buf4_sort(&C, PSIF_CC_CINTS, qpsr, ID("[V,O]"), ID("[V,O]"), "C <ai|bj>");
+  global_dpd_->buf4_close(&C);
+
+  // Close up all DPD files
+  for(int i=PSIF_CC_MIN; i < PSIF_CC_TMP; i++) psio->close(i,1);
+  for(int i=PSIF_CC_TMP; i <= PSIF_CC_TMP11; i++) psio->close(i,0); /* delete CC_TMP files */
+  for(int i=PSIF_CC_TMP11+1; i <= PSIF_CC_MAX; i++) psio->close(i,1);
 
   return Success;
 }
